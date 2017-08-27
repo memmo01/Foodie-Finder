d3039ae40cab708150d95d8d060a63122a94e341
michael emmons <memmons18@gmail.com>
2017-08-27 12:52:20 -0400

    added folders so code is easier to follow with routing. added new images to profile. have compatibility profiles populating after submitting a survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,22 +5,111 @@
 
 //this will need to use body parser!!
 
+var foodies = require("../data/friends.js");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type: "application/vnd.api+json"}));
+var data = [ { name: 'Joey Bag O\'Donuts',
+    picURL: 'http://www.motherjones.com/files/istock_000010544064_large.jpg',
+    q1: '3',
+    q2: '1',
+    q3: '2',
+    q4: '2',
+    q5: '4',
+    q6: '4',
+    q7: '2',
+    q8: '3',
+    q9: '2',
+    q10: '2' },
+  { name: 'Jonny S',
+    picURL: 'http://s.eatthis-cdn.com/media/images/ext/789553511/man-eating-spaghetti-foods-make-you-hungrier.jpg',
+    q1: '3',
+    q2: '2',
+    q3: '3',
+    q4: '2',
+    q5: '4',
+    q6: '2',
+    q7: '3',
+    q8: '4',
+    q9: '2',
+    q10: '4' },
+  { name: 'April O\'neil',
+    picURL: 'http://i.dailymail.co.uk/i/pix/2008/10/20/article-0-026F5DDC00000578-526_468x318.jpg',
+    q1: '5',
+    q2: '5',
+    q3: '5',
+    q4: '5',
+    q5: '5',
+    q6: '5',
+    q7: '5',
+    q8: '5',
+    q9: '5',
+    q10: '5' },
+  { name: 'Mary M',
+    picURL: 'http://www.youngandraw.com/wp-content/uploads/Eating-Raw-Can-Make-You-Healthier.jpg',
+    q1: '4',
+    q2: '2',
+    q3: '5',
+    q4: '2',
+    q5: '3',
+    q6: '4',
+    q7: '2',
+    q8: '5',
+    q9: '4',
+    q10: '2' },
+  { name: 'Joel L',
+    picURL: 'https://www.bodybuilding.com/fun/images/2014/50-fat-torching-tricks-3.jpg',
+    q1: '4',
+    q2: '4',
+    q3: '4',
+    q4: '4',
+    q5: '4',
+    q6: '4',
+    q7: '4',
+    q8: '4',
+    q9: '4',
+    q10: '4' },
+  { name: 'Lorie G',
+    picURL: 'https://i.pinimg.com/236x/7a/e9/c6/7ae9c6b27b2e6729f6558a786edfedca.jpg',
+    q1: '4',
+    q2: '2',
+    q3: '2',
+    q4: '2',
+    q5: '5',
+    q6: '2',
+    q7: '2',
+    q8: '3',
+    q9: '5',
+    q10: '5' },
+  { name: 'jo mama',
+    picURL: 'https://huenemefamily.com/wp-content/uploads/2016/06/food.jpg',
+    q1: '3',
+    q2: '3',
+    q3: '3',
+    q4: '3',
+    q5: '3',
+    q6: '3',
+    q7: '3',
+    q8: '3',
+    q9: '3',
+    q10: '3' } ];
 
-var data = [""];
 
-app.get ("/api/friends", function(req,res){
-    var friends = [];
-    for(i=0;i<res.length;i ++)
-    {
-        if(res[i].num < 4 )
-        {
-            friends.push(res[i]);
-        }
-        return res.json(freinds);
-}
-})
+module.exports = function(app){
+app.get("/api/users",function(req,res){
+    var users=[];
+    for(var i = 0; i < data.length; i++){
+        users.push(data[i]);
+    }
+        
+    return res.json(users);
+});
+
+app.post("/api/users", function(req,res){
+    data.push(req.body);
+    console.log(data);
+
+    res.json({
+        complete: true
+    });
+});
+};
+        
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // we've used in class: express, body-parser and path.
 
 var express = require("express");
-var path = require("path");
+
 var bodyParser = require("body-parser");
 
 var app = express();
@@ -12,49 +12,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use('/app', express.static('app'))
 
-var data = [{
-  name: "Joey Bag O'Donuts",
-  picURL: "http://www.motherjones.com/files/istock_000010544064_large.jpg",
-  q1: "3",
-  q2: "1",
-  q3: "2",
-  q4: "2",
-  q5: "4",
-  q6: "4",
-  q7: "2",
-  q8: "3",
-  q9: "2",
-  q10: "2"
-}];
-
-app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname, "./app/public/home.html"));
-});
-
-app.get("/survey", function(req,res){
-    res.sendFile(path.join(__dirname, "./app/public/survery.html"));
-});
-
-app.get("/api/users",function(req,res){
-    var users=[];
-    for(var i = 0; i < data.length; i++){
-        users.push(data[i]);
-    }
-        
-    return res.json(users);
-});
+require ("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 
 
-app.post("/api/users", function(req,res){
-    data.push(req.body);
-    console.log(data);
 
-    res.json({
-        complete: true
-    });
-});
 
 app.listen(PORT,function(){
     console.log("server listening");
